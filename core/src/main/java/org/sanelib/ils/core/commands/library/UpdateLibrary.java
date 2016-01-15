dde863d2168a543ce279139b0d677795ea63292f
srajwadi <sunny@leafnode.io>
2016-01-16 04:19:45 +0530

    Adding Update Library and Delegate.

---
+++ core/src/main/java/org/sanelib/ils/core/commands/library/UpdateLibrary.java
@@ -0,0 +1,10 @@
+package org.sanelib.ils.core.commands.library;
+
+public class UpdateLibrary extends AddLibrary {
+
+    @Override
+    public String toString() {
+        return "UpdateLibrary{} " + super.toString();
+    }
+}
+
