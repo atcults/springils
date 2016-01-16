19e1cf4c1c0dda202678a8500927abccc4cc3df4
srajwadi <sunny@leafnode.io>
2016-01-16 17:00:30 +0530

    Adding Agency Repository.

---
+++ core/src/main/java/org/sanelib/ils/core/dao/AgencyRepository.java
@@ -0,0 +1,8 @@
+package org.sanelib.ils.core.dao;
+
+import org.sanelib.ils.core.domain.entity.Agency;
+
+@org.springframework.stereotype.Repository
+public class AgencyRepository extends EntityRepositoryBase<Agency> {
+
+}
