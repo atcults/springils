06a5997992d1e58c9b790b77136a419c62d83209
srajwadi <sunny@leafnode.io>
2016-01-17 14:37:47 +0530

    Adding Agency Service.

--- api/src/main/java/org/sanelib/ils/api/services/ApiEndPointConstants.java
+++ api/src/main/java/org/sanelib/ils/api/services/ApiEndPointConstants.java
@@ -5,5 +5,6 @@ public class ApiEndPointConstants {
     public class Admin{
         public static final String PUBLISHER_END_POINT = "/publisher";
         public static final String LIBRARY_END_POINT = "/library";
+        public static final String AGENCY_END_POINT = "/agency";
     }
 }
---
+++ api/src/main/java/org/sanelib/ils/api/services/agency/AgencyService.java
@@ -0,0 +1,48 @@
+package org.sanelib.ils.api.services.agency;
+
+import org.sanelib.ils.api.dto.agency.AgencyDto;
+import org.sanelib.ils.api.services.ApiEndPointConstants;
+import org.sanelib.ils.api.services.ApiServiceBase;
+import org.sanelib.ils.core.activities.ActivitiProcessConstants;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Path(ApiEndPointConstants.Admin.AGENCY_END_POINT)
+@Produces(MediaType.APPLICATION_JSON)
+public class AgencyService extends ApiServiceBase {
+
+    @GET
+    public List getAllAgencies() throws Throwable {
+        List<AgencyDto> list = new ArrayList<>();
+        AgencyDto agencyDto = new AgencyDto();
+        agencyDto.setId("1");
+        agencyDto.setLibraryId("1");
+        agencyDto.setName("Agency Name 1");
+        list.add(agencyDto);
+        return list;
+    }
+
+    @POST
+    public String addAgency(AgencyDto dto) throws Throwable {
+        return execute(dto, ActivitiProcessConstants.Admin.ADD_AGENCY);
+    }
+
+    @PUT
+    public String updateAgency(AgencyDto dto) throws Throwable {
+        return execute(dto, ActivitiProcessConstants.Admin.UPDATE_AGENCY);
+    }
+
+    @DELETE
+    @Path("/{libraryId}/{id}")
+    public String deleteAgency(@PathParam("libraryId") String libraryId, @PathParam("id") String id) throws Throwable {
+        AgencyDto agencyDto = new AgencyDto();
+        agencyDto.setLibraryId(libraryId);
+        agencyDto.setId(id);
+        return execute(agencyDto, ActivitiProcessConstants.Admin.DELETE_AGENCY);
+    }
+}
