36bbe42af790ac5aee224899f7ca71b4a435fef1
amrita8791 <amrita.gianchandani@sanelib.com>
2016-01-20 17:23:39 +0530

    Adding FiscalYear Service. Updating ApiEndPointConstants.

--- api/src/main/java/org/sanelib/ils/api/services/ApiEndPointConstants.java
+++ api/src/main/java/org/sanelib/ils/api/services/ApiEndPointConstants.java
@@ -6,5 +6,6 @@ public class ApiEndPointConstants {
         public static final String PUBLISHER_END_POINT = "/publisher";
         public static final String LIBRARY_END_POINT = "/library";
         public static final String AGENCY_END_POINT = "/agency";
+        public static final String FISCALYEAR_END_POINT = "/fiscalyear";
     }
 }
