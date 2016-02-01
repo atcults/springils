3e65587fc2d8ecedb60c838d5964678c32e1014b
piusanelib <disha.sil@sanelib.com>
2016-02-01 11:42:00 +0530

    Adding PatronDto and add, update and delete converter of the patron

---
+++ api/src/main/java/org/sanelib/ils/api/converters/patron/AddPatronConverter.java
@@ -0,0 +1,156 @@
+package org.sanelib.ils.api.converters.patron;
+
+
+import org.sanelib.ils.api.converters.ConverterHelper;
+import org.sanelib.ils.api.converters.DtoToCommandConverter;
+import org.sanelib.ils.api.dto.patron.PatronDto;
+import org.sanelib.ils.common.utils.DateHelper;
+import org.sanelib.ils.common.utils.RegularExpressionHelper;
+import org.sanelib.ils.core.commands.ProcessCommand;
+import org.sanelib.ils.core.commands.patron.AddPatron;
+import org.sanelib.ils.core.enums.PatronType;
+import org.sanelib.ils.core.exceptions.ProcessError;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AddPatronConverter implements DtoToCommandConverter<PatronDto> {
+    @Override
+    public ProcessCommand convert(PatronDto dto, ProcessError processError) throws NoSuchFieldException, IllegalAccessException {
+
+        AddPatron command = new AddPatron();
+
+        ConverterHelper.checkLibraryIdRequired(dto, command, processError);
+        ConverterHelper.checkCodeRequired(dto,command , processError);
+
+        command.setPatronCategoryId(Integer.parseInt(dto.getPatronCategoryId()));
+        command.setIsOnline(dto.getIsOnline());
+        command.setOwns(dto.getOwns());
+
+        if(!RegularExpressionHelper.checkDateFormat(dto.getCreatedOn())) {
+            processError.addError("common.field.pattern", "createdOn", "domain.patron.createdOn", RegularExpressionHelper.DATE_FORMAT_EXAMPLE);
+        } else {
+            command.setCreatedOn(DateHelper.fromDateString(dto.getCreatedOn()));
+        }
+
+        command.setOtherLibraryPatronId(Integer.parseInt(dto.getOtherLibraryPatronId()));
+        command.setLibraryPatronId(Integer.parseInt(dto.getLibraryPatronId()));
+        command.setDeptId(Integer.parseInt(dto.getDeptId()));
+        command.setFirstName(dto.getFirstName());
+        command.setAddress1(dto.getAddress1());
+        command.setAddress2(dto.getAddress2());
+        command.setCity(dto.getCity());
+        command.setState(dto.getState());
+        command.setCountry(dto.getCountry());
+        command.setPin(dto.getPin());
+
+        if(!RegularExpressionHelper.checkPhoneFormat(dto.getPhone1())) {
+            processError.addError("common.field.pattern", "phone1", "domain.patron.phone1", RegularExpressionHelper.PHONE_FORMAT_EXAMPLE);
+        } else {
+            command.setPhone1(dto.getPhone1());
+        }
+
+        if(!RegularExpressionHelper.checkPhoneFormat(dto.getPhone2())) {
+            processError.addError("common.field.pattern", "phone2", "domain.patron.phone2", RegularExpressionHelper.PHONE_FORMAT_EXAMPLE);
+        } else {
+            command.setPhone2(dto.getPhone2());
+        }
+
+        if(!RegularExpressionHelper.checkPhoneFormat(dto.getFax())) {
+            processError.addError("common.field.pattern", "fax", "domain.patron.fax", RegularExpressionHelper.PHONE_FORMAT_EXAMPLE);
+        } else {
+            command.setFax(dto.getFax());
+        }
+
+        if(!RegularExpressionHelper.checkEmailFormat(dto.getEmail())) {
+            processError.addError("common.field.pattern", "email", "domain.patron.email", RegularExpressionHelper.EMAIL_FORMAT_EXAMPLE);
+        } else {
+            command.setEmail(dto.getEmail());
+        }
+
+        command.setPermanentAddress1(dto.getPermanentAddress1());
+        command.setPermanentAddress2(dto.getPermanentAddress2());
+        command.setPermanentCity(dto.getPermanentCity());
+        command.setPermanentState(dto.getPermanentState());
+        command.setPermanentCountry(dto.getPermanentCountry());
+        command.setPermanentPin(dto.getPermanentPin());
+
+        if(!RegularExpressionHelper.checkPhoneFormat(dto.getPermanentPhone1())) {
+            processError.addError("common.field.pattern", "permanentPhone1", "domain.patron.permanentPhone1", RegularExpressionHelper.PHONE_FORMAT_EXAMPLE);
+        } else {
+            command.setPermanentPhone1(dto.getPermanentPhone1());
+        }
+
+        if(!RegularExpressionHelper.checkPhoneFormat(dto.getPermanentPhone2())) {
+            processError.addError("common.field.pattern", "permanentPhone2", "domain.patron.permanentPhone2", RegularExpressionHelper.PHONE_FORMAT_EXAMPLE);
+        } else {
+            command.setPermanentPhone2(dto.getPermanentPhone2());
+        }
+
+        if(!RegularExpressionHelper.checkPhoneFormat(dto.getPermanentFax())) {
+            processError.addError("common.field.pattern", "permanentFax", "domain.patron.permanentFax", RegularExpressionHelper.PHONE_FORMAT_EXAMPLE);
+        } else {
+            command.setPermanentFax(dto.getPermanentFax());
+        }
+
+        if(!RegularExpressionHelper.checkEmailFormat(dto.getPermanentEmail())) {
+            processError.addError("common.field.pattern", "permanentEmail", "domain.patron.permanentEmail", RegularExpressionHelper.EMAIL_FORMAT_EXAMPLE);
+        } else {
+            command.setPermanentEmail(dto.getPermanentEmail());
+        }
+
+        if(!RegularExpressionHelper.checkDateFormat(dto.getMembershipFrom())) {
+            processError.addError("common.field.pattern", "membershipFrom", "domain.patron.membershipFrom", RegularExpressionHelper.DATE_FORMAT_EXAMPLE);
+        } else {
+            command.setMembershipFrom(DateHelper.fromDateString(dto.getMembershipFrom()));
+        }
+
+        if(!RegularExpressionHelper.checkDateFormat(dto.getMembershipTo())) {
+            processError.addError("common.field.pattern", "membershipTo", "domain.patron.membershipTo", RegularExpressionHelper.DATE_FORMAT_EXAMPLE);
+        } else {
+            command.setMembershipTo(DateHelper.fromDateString(dto.getMembershipTo()));
+        }
+
+        command.setDelinquencyReason(dto.getDelinquencyReason());
+
+        if(ConverterHelper.checkRequiredLength(dto.getCommonEmail(), 1, "commonEmail", "domain.patron.commonEmail", processError)){
+            command.setCommonEmail(dto.getCommonEmail());
+        }
+
+        if(ConverterHelper.checkRequiredLength(dto.getCommonInstantMsg(), 1, "commonInstantMsg", "domain.patron.commonInstantMsg", processError)){
+            command.setCommonInstantMsg(dto.getCommonInstantMsg());
+        }
+
+        if(ConverterHelper.checkRequiredLength(dto.getCommonPrint(), 1, "commonPrint", "domain.patron.commonPrint", processError)){
+            command.setCommonPrint(dto.getCommonPrint());
+        }
+
+        if(!RegularExpressionHelper.checkDateFormat(dto.getEntryDate())) {
+            processError.addError("common.field.pattern", "entryDate", "domain.patron.entryDate", RegularExpressionHelper.DATE_FORMAT_EXAMPLE);
+        } else {
+            command.setEntryDate(DateHelper.fromDateString(dto.getEntryDate()));
+        }
+
+        command.setUserPassword(dto.getUserPassword());
+        command.setCourseId(Integer.parseInt(dto.getCourseId()));
+        command.setSendToAddress(dto.getSendToAddress());
+        command.setCustom(dto.getCustom());
+        command.setPrivilege(dto.getPrivilege());
+        command.setTwitterId(dto.getTwitterId());
+        command.setFacebookId(dto.getFacebookId());
+        command.setSubLocationId(Integer.parseInt(dto.getSubLocationId()));
+        command.setLoginId(dto.getLoginId());
+
+        if(ConverterHelper.checkRequiredLength(dto.getAuthenticateLocalDatabase(), 1, "authenticateLocalDatabase", "domain.patron.authenticateLocalDatabase", processError)){
+           command.setAuthenticateLocalDatabase(dto.getAuthenticateLocalDatabase());
+       }
+        PatronType patronType = PatronType.getByValue(String.valueOf(dto.getPatronType()));
+
+        if(!patronType.toString().equals("A") && !patronType.toString().equals("B")){
+            processError.addError("common.field.select", "patronType", "domain.patron.patronType");
+        }else {
+            command.setPatronType(patronType);
+        }
+
+        return command;
+    }
+}
---
+++ api/src/main/java/org/sanelib/ils/api/dto/patron/PatronDto.java
@@ -0,0 +1,467 @@
+package org.sanelib.ils.api.dto.patron;
+
+
+import org.sanelib.ils.api.dto.DtoWithCode;
+import org.sanelib.ils.api.dto.DtoWithLibraryId;
+import org.sanelib.ils.core.enums.PatronType;
+
+public class PatronDto implements DtoWithCode, DtoWithLibraryId {
+
+    private String libraryId;
+    private String code;
+    private String patronCategoryId;
+    private String isOnline;
+    private String owns;
+    private String createdOn;
+    private String otherLibraryPatronId;
+    private String libraryPatronId;
+    private PatronType patronType;
+    private String deptId;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String address1;
+    private String address2;
+    private String city;
+    private String state;
+    private String country;
+    private String pin;
+    private String phone1;
+    private String phone2;
+    private String fax;
+    private String email;
+    private String permanentAddress1;
+    private String permanentAddress2;
+    private String permanentCity;
+    private String permanentState;
+    private String permanentCountry;
+    private String permanentPin;
+    private String permanentPhone1;
+    private String permanentPhone2;
+    private String permanentFax;
+    private String permanentEmail;
+    private String membershipFrom;
+    private String membershipTo;
+    private String delinquencyReason;
+    private String commonEmail;
+    private String commonInstantMsg;
+    private String commonPrint;
+    private String entryDate;
+    private String userPassword;
+    private String courseId;
+    private String status;
+    private String sendToAddress;
+    private String custom;
+    private String privilege;
+    private String twitterId;
+    private String facebookId;
+    private String subLocationId;
+    private String loginId;
+    private String authenticateLocalDatabase;
+
+    public String getLibraryId() {
+        return libraryId;
+    }
+
+    public void setLibraryId(String libraryId) {
+        this.libraryId = libraryId;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getPatronCategoryId() {
+        return patronCategoryId;
+    }
+
+    public void setPatronCategoryId(String patronCategoryId) {
+        this.patronCategoryId = patronCategoryId;
+    }
+
+    public String getIsOnline() {
+        return isOnline;
+    }
+
+    public void setIsOnline(String isOnline) {
+        this.isOnline = isOnline;
+    }
+
+    public String getOwns() {
+        return owns;
+    }
+
+    public void setOwns(String owns) {
+        this.owns = owns;
+    }
+
+    public String getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(String createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public String getOtherLibraryPatronId() {
+        return otherLibraryPatronId;
+    }
+
+    public void setOtherLibraryPatronId(String otherLibraryPatronId) {
+        this.otherLibraryPatronId = otherLibraryPatronId;
+    }
+
+    public String getLibraryPatronId() {
+        return libraryPatronId;
+    }
+
+    public void setLibraryPatronId(String libraryPatronId) {
+        this.libraryPatronId = libraryPatronId;
+    }
+
+    public PatronType getPatronType() {
+        return patronType;
+    }
+
+    public void setPatronType(PatronType patronType) {
+        this.patronType = patronType;
+    }
+
+    public String getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(String deptId) { this.deptId = deptId;  }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    public String getPhone1() {
+        return phone1;
+    }
+
+    public void setPhone1(String phone1) {
+        this.phone1 = phone1;
+    }
+
+    public String getPhone2() {
+        return phone2;
+    }
+
+    public void setPhone2(String phone2) {
+        this.phone2 = phone2;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPermanentAddress1() {
+        return permanentAddress1;
+    }
+
+    public void setPermanentAddress1(String permanentAddress1) {
+        this.permanentAddress1 = permanentAddress1;
+    }
+
+    public String getPermanentAddress2() {
+        return permanentAddress2;
+    }
+
+    public void setPermanentAddress2(String permanentAddress2) {
+        this.permanentAddress2 = permanentAddress2;
+    }
+
+    public String getPermanentCity() {
+        return permanentCity;
+    }
+
+    public void setPermanentCity(String permanentCity) {
+        this.permanentCity = permanentCity;
+    }
+
+    public String getPermanentState() {
+        return permanentState;
+    }
+
+    public void setPermanentState(String permanentState) {
+        this.permanentState = permanentState;
+    }
+
+    public String getPermanentCountry() {
+        return permanentCountry;
+    }
+
+    public void setPermanentCountry(String permanentCountry) {
+        this.permanentCountry = permanentCountry;
+    }
+
+    public String getPermanentPin() {
+        return permanentPin;
+    }
+
+    public void setPermanentPin(String permanentPin) {
+        this.permanentPin = permanentPin;
+    }
+
+    public String getPermanentPhone1() {
+        return permanentPhone1;
+    }
+
+    public void setPermanentPhone1(String permanentPhone1) {
+        this.permanentPhone1 = permanentPhone1;
+    }
+
+    public String getPermanentPhone2() {
+        return permanentPhone2;
+    }
+
+    public void setPermanentPhone2(String permanentPhone2) {
+        this.permanentPhone2 = permanentPhone2;
+    }
+
+    public String getPermanentFax() {
+        return permanentFax;
+    }
+
+    public void setPermanentFax(String permanentFax) {
+        this.permanentFax = permanentFax;
+    }
+
+    public String getPermanentEmail() {
+        return permanentEmail;
+    }
+
+    public void setPermanentEmail(String permanentEmail) {
+        this.permanentEmail = permanentEmail;
+    }
+
+    public String getMembershipFrom() {
+        return membershipFrom;
+    }
+
+    public void setMembershipFrom(String membershipFrom) {
+        this.membershipFrom = membershipFrom;
+    }
+
+    public String getMembershipTo() {
+        return membershipTo;
+    }
+
+    public void setMembershipTo(String membershipTo) {
+        this.membershipTo = membershipTo;
+    }
+
+    public String getDelinquencyReason() {
+        return delinquencyReason;
+    }
+
+    public void setDelinquencyReason(String delinquencyReason) {
+        this.delinquencyReason = delinquencyReason;
+    }
+
+    public String getCommonEmail() {
+        return commonEmail;
+    }
+
+    public void setCommonEmail(String commonEmail) {
+        this.commonEmail = commonEmail;
+    }
+
+    public String getCommonInstantMsg() {
+        return commonInstantMsg;
+    }
+
+    public void setCommonInstantMsg(String commonInstantMsg) {
+        this.commonInstantMsg = commonInstantMsg;
+    }
+
+    public String getCommonPrint() {
+        return commonPrint;
+    }
+
+    public void setCommonPrint(String commonPrint) {
+        this.commonPrint = commonPrint;
+    }
+
+    public String getEntryDate() {
+        return entryDate;
+    }
+
+    public void setEntryDate(String entryDate) {
+        this.entryDate = entryDate;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(String courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getSendToAddress() {
+        return sendToAddress;
+    }
+
+    public void setSendToAddress(String sendToAddress) {
+        this.sendToAddress = sendToAddress;
+    }
+
+    public String getCustom() {
+        return custom;
+    }
+
+    public void setCustom(String custom) {
+        this.custom = custom;
+    }
+
+    public String getPrivilege() {
+        return privilege;
+    }
+
+    public void setPrivilege(String privilege) {
+        this.privilege = privilege;
+    }
+
+    public String getTwitterId() {
+        return twitterId;
+    }
+
+    public void setTwitterId(String twitterId) {
+        this.twitterId = twitterId;
+    }
+
+    public String getFacebookId() {
+        return facebookId;
+    }
+
+    public void setFacebookId(String facebookId) {
+        this.facebookId = facebookId;
+    }
+
+    public String getSubLocationId() {
+        return subLocationId;
+    }
+
+    public void setSubLocationId(String subLocationId) {
+        this.subLocationId = subLocationId;
+    }
+
+    public String getLoginId() {
+        return loginId;
+    }
+
+    public void setLoginId(String loginId) {
+        this.loginId = loginId;
+    }
+
+    public String getAuthenticateLocalDatabase() {
+        return authenticateLocalDatabase;
+    }
+
+    public void setAuthenticateLocalDatabase(String authenticateLocalDatabase) {
+        this.authenticateLocalDatabase = authenticateLocalDatabase;
+    }
+}
