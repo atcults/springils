1d7d30a19cced17a9917f5bde13dcbec725c8128
piusanelib <disha.sil@sanelib.com>
2016-02-02 17:06:58 +0530

    Updating converter tests for price in bindingType

--- api/src/test/java/org/sanelib/ils/api/converters/bindingType/AddBindingTypeConverterTest.java
+++ api/src/test/java/org/sanelib/ils/api/converters/bindingType/AddBindingTypeConverterTest.java
@@ -1,12 +1,9 @@
 package org.sanelib.ils.api.converters.bindingType;
 
 import org.junit.Test;
-import org.sanelib.ils.api.converters.agency.AddAgencyConverter;
-import org.sanelib.ils.api.dto.agency.AgencyDto;
 import org.sanelib.ils.api.dto.bindingType.BindingTypeDto;
 import org.sanelib.ils.common.utils.DateHelper;
 import org.sanelib.ils.core.commands.ProcessCommand;
-import org.sanelib.ils.core.commands.agency.AddAgency;
 import org.sanelib.ils.core.commands.bindingType.AddBindingType;
 import org.sanelib.ils.core.exceptions.ProcessError;
 
@@ -21,7 +18,7 @@ public class AddBindingTypeConverterTest {
 
         dto.setLibraryId("1");
         dto.setBindType("BindType");
-        dto.setPrice("500");
+        dto.setPrice("500.00");
         dto.setEntryId("EntryId01");
         dto.setEntryDate("2007/01/02");
 
@@ -36,7 +33,7 @@ public class AddBindingTypeConverterTest {
         AddBindingType addBindingType = (AddBindingType) command;
         assertEquals("Library Id is not mapped", dto.getLibraryId(), String.valueOf(addBindingType.getLibraryId()));
         assertEquals("Bind type is not mapped", dto.getBindType(), addBindingType.getBindType());
-        assertEquals("Price is not mapped", dto.getPrice(), String.valueOf(addBindingType.getPrice()));
+        assertEquals("Price is not mapped", Double.valueOf(dto.getPrice()), addBindingType.getPrice());
         assertEquals("Entry Id is not mapped", dto.getEntryId(), addBindingType.getEntryId());
         assertEquals("Entry date is not mapped", dto.getEntryDate(), DateHelper.toDateString(addBindingType.getEntryDate()));
 
--- api/src/test/java/org/sanelib/ils/api/converters/bindingType/UpdateBindingTypeConverterTest.java
+++ api/src/test/java/org/sanelib/ils/api/converters/bindingType/UpdateBindingTypeConverterTest.java
@@ -2,12 +2,9 @@ package org.sanelib.ils.api.converters.bindingType;
 
 
 import org.junit.Test;
-import org.sanelib.ils.api.converters.agency.UpdateAgencyConverter;
-import org.sanelib.ils.api.dto.agency.AgencyDto;
 import org.sanelib.ils.api.dto.bindingType.BindingTypeDto;
 import org.sanelib.ils.common.utils.DateHelper;
 import org.sanelib.ils.core.commands.ProcessCommand;
-import org.sanelib.ils.core.commands.agency.UpdateAgency;
 import org.sanelib.ils.core.commands.bindingType.UpdateBindingType;
 import org.sanelib.ils.core.exceptions.ProcessError;
 
@@ -22,7 +19,7 @@ public class UpdateBindingTypeConverterTest {
         dto.setId("1");
         dto.setLibraryId("1");
         dto.setBindType("BindType");
-        dto.setPrice("500");
+        dto.setPrice("500.00");
         dto.setEntryId("EntryId01");
         dto.setEntryDate("2007/01/02");
 
@@ -37,7 +34,7 @@ public class UpdateBindingTypeConverterTest {
         assertEquals("Id is not mapped", dto.getId(), String.valueOf(updateBindingType.getId()));
         assertEquals("Library Id is not mapped", dto.getLibraryId(), String.valueOf(updateBindingType.getLibraryId()));
         assertEquals("Bind type is not mapped", dto.getBindType(), updateBindingType.getBindType());
-        assertEquals("Price is not mapped", dto.getPrice(), String.valueOf(updateBindingType.getPrice()));
+        assertEquals("Price is not mapped", Double.valueOf(dto.getPrice()), updateBindingType.getPrice());
         assertEquals("Entry Id is not mapped", dto.getEntryId(), updateBindingType.getEntryId());
         assertEquals("Entry date is not mapped", dto.getEntryDate(), DateHelper.toDateString(updateBindingType.getEntryDate()));
     }
