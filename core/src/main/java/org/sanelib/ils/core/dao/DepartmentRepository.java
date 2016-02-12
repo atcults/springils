57b718a04ffda6e3e8d3597f97b9e566292ebdf5
PMSanelib <priya.mehta@sanelib.com>
2016-02-13 01:11:40 +0530

    Adding department repository

---
+++ core/src/main/java/org/sanelib/ils/core/dao/DepartmentRepository.java
@@ -0,0 +1,8 @@
+package org.sanelib.ils.core.dao;
+
+import org.sanelib.ils.core.domain.entity.Department;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DepartmentRepository extends EntityRepositoryBase<Department> {
+}
