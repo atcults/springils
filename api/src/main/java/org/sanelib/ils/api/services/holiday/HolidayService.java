6d399514f6d29ba6dda9261c40fde5013708d033
srajwadi <sunny@leafnode.io>
2016-02-13 00:41:53 +0530

    Fix Compilation errors.

--- api/src/main/java/org/sanelib/ils/api/services/holiday/HolidayService.java
+++ api/src/main/java/org/sanelib/ils/api/services/holiday/HolidayService.java
@@ -4,7 +4,6 @@ import org.sanelib.ils.api.dto.holiday.HolidayDto;
 import org.sanelib.ils.api.services.ApiEndPointConstants;
 import org.sanelib.ils.api.services.ApiServiceBase;
 import org.sanelib.ils.core.activities.ActivitiProcessConstants;
-import org.sanelib.ils.core.enums.HolidayType;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.GET;
@@ -31,7 +30,7 @@ public class HolidayService extends ApiServiceBase {
         holidayDto.setFiscalYearId("20152016");
         holidayDto.setStartDate("2016/02/09");
         holidayDto.setEndDate("2016/02/29");
-        holidayDto.setHolidayType(HolidayType.getByName("Specific"));
+        holidayDto.setHolidayType("Specific");
         holidayDto.setNote(null);
 
         dtos.add(holidayDto);
@@ -43,6 +42,4 @@ public class HolidayService extends ApiServiceBase {
     public String addHoliday(HolidayDto holidayDto) throws Throwable {
         return execute(holidayDto, ActivitiProcessConstants.Admin.ADD_HOLIDAY);
     }
-
-
 }
