ada8e3ee1b2984aa1b34ce2841be0fa1fce550ba
piusanelib <disha.sil@sanelib.com>
2016-02-15 17:17:34 +0530

    Adding Binding type View repository. And binding Type  mapper

---
+++ core/src/main/java/org/sanelib/ils/core/dao/read/admin/BindingTypeViewRepository.java
@@ -0,0 +1,31 @@
+package org.sanelib.ils.core.dao.read.admin;
+
+import org.sanelib.ils.core.dao.read.ViewService;
+import org.sanelib.ils.core.dao.read.ViewServiceBase;
+import org.sanelib.ils.core.dao.read.admin.mapper.BindingTypeMapper;
+import org.sanelib.ils.core.domain.view.admin.LibraryView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.List;
+
+@SuppressWarnings("unchecked")
+@Component
+public class BindingTypeViewRepository extends ViewServiceBase {
+
+    @Autowired
+    BindingTypeMapper mapper;
+
+    @Autowired
+    ViewService viewService;
+
+    @Override
+    protected List<String> getStatements() {
+        return Collections.singletonList("select * from cir_co_bind_types");
+    }
+
+   public List<LibraryView> getAll() throws Throwable {
+        return viewService.loadQuery(getStatements().get(0), mapper);
+    }
+}
---
+++ core/src/main/java/org/sanelib/ils/core/dao/read/admin/mapper/BindingTypeMapper.java
@@ -0,0 +1,27 @@
+package org.sanelib.ils.core.dao.read.admin.mapper;
+
+import org.sanelib.ils.core.dao.read.DataResultSet;
+import org.sanelib.ils.core.dao.read.ViewMapper;
+import org.sanelib.ils.core.domain.view.admin.BindingTypeView;
+import org.springframework.stereotype.Component;
+
+import java.sql.SQLException;
+
+@Component
+public class BindingTypeMapper implements ViewMapper<BindingTypeView> {
+
+    public BindingTypeView map(final DataResultSet rs) throws SQLException {
+
+        final String viewName = "binding_type";
+
+        final BindingTypeView view = new BindingTypeView();
+        view.setLibraryId(rs.getInt(viewName, "library_id"));
+        view.setId(rs.getInt(viewName, "bind_type_id"));
+        view.setBindType(rs.getString(viewName, "bind_type"));
+        view.setPrice(rs.getDouble(viewName, "price"));
+        view.setEntryId(rs.getString(viewName, "entry_id"));
+        view.setEntryDate(rs.getDate(viewName, "entry_date"));
+        return view;
+    }
+}
+
