61bee10e38616a1a5ccb1f2758805ec9e8cd02e4
abhiptl <abhishek.patel@sanelib.com>
2016-02-17 11:03:28 +0530

    LoggingAspect will be enable only when enabling this by adding this annotation. We can plug-in plug-out all out aspects by removing this annotation.

--- api/src/main/java/org/sanelib/ils/ApiMain.java
+++ api/src/main/java/org/sanelib/ils/ApiMain.java
@@ -11,12 +11,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 
 import javax.persistence.EntityManagerFactory;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class ApiMain implements CommandLineRunner {
 
 	@Autowired
