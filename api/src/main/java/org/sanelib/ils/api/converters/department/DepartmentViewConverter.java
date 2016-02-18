4976a2c3eeb3ae4d78812920570edd5d058b5135
piusanelib <disha.sil@sanelib.com>
2016-02-18 18:39:47 +0530

    Updated DepartmentConverter to use AbstractViewToDtoConverterImpl

--- api/src/main/java/org/sanelib/ils/api/converters/department/DepartmentViewConverter.java
+++ api/src/main/java/org/sanelib/ils/api/converters/department/DepartmentViewConverter.java
@@ -1,13 +1,13 @@
 package org.sanelib.ils.api.converters.department;
 
-import org.sanelib.ils.api.converters.ViewToDtoConverter;
+import org.sanelib.ils.api.converters.AbstractViewToDtoConverterImpl;
 import org.sanelib.ils.api.dto.department.DepartmentDto;
 import org.sanelib.ils.common.utils.DateHelper;
 import org.sanelib.ils.core.domain.view.admin.DepartmentView;
 import org.springframework.stereotype.Component;
 
 @Component
-public class DepartmentViewConverter implements ViewToDtoConverter<DepartmentDto, DepartmentView> {
+public class DepartmentViewConverter extends AbstractViewToDtoConverterImpl<DepartmentDto, DepartmentView> {
 
     @Override
     public DepartmentDto convert(DepartmentView departmentView) {
