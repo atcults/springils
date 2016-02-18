e5be1d067e72900beff176f3417c1b8821a45af7
abhiptl <abhishek.patel@sanelib.com>
2016-02-18 11:32:02 +0530

    Initial implementation of oauth users to store basic user information. This domain may be replaced by ngl patron.

---
+++ oauth/src/main/java/org/sanelib/ils/auth/domain/UserDTO.java
@@ -0,0 +1,30 @@
+package org.sanelib.ils.auth.domain;
+
+public class UserDTO {
+
+	private String userID;
+	private String userName;
+	private String passWord;
+	
+	public UserDTO(){}
+	
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassWord() {
+		return passWord;
+	}
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+	
+}
