5d455d70322d0661f60b16242e7e67c26538e4ef
abhiptl <abhishek.patel@sanelib.com>
2016-02-19 14:30:39 +0530

    Updating mapper to have changes to table name prefix with oauth. Using patron table for identification of user.

--- oauth/src/main/java/org/sanelib/ils/auth/dao/OAuth2Mapper.java
+++ oauth/src/main/java/org/sanelib/ils/auth/dao/OAuth2Mapper.java
@@ -14,27 +14,45 @@ import org.sanelib.ils.auth.domain.UserDTO;
 
 public interface OAuth2Mapper {
 
-	@Select("select client_id from client where client_id=#{client_id}")
+	@Select("select client_id from oauth_client where client_id=#{client_id}")
 	public String isClientIdValid(@Param("client_id") String clientId);
 	
-	@Select("select client_secret from client where client_secret=#{client_secret}")
+	@Select("select client_secret from oauth_client where client_secret=#{client_secret}")
 	public String isClientSecretValid(@Param("client_secret") final String clientSecret);
 
-	@Select("select userid from users where username=#{username} and password=#{password}")
-	public String isUserValid(@Param("username") final String userName, @Param("password") final String password);
-	
-	
-	@Select("select * from users where username=#{username} and password=#{password}")
+
+	@Select("select * from patron where patron_id=#{username} and user_password=#{password}")
 	@Results({
-		@Result(column = "userid", property = "userID", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "username", property = "userName", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "password", property = "passWord", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "library_id", property = "libraryId", jdbcType = JdbcType.INTEGER),
+		@Result(column = "patron_id", property = "patronId", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "patron_category_id", property = "patronCategoryId", jdbcType = JdbcType.INTEGER),
+		@Result(column = "library_patron_id", property = "libraryPatronId", jdbcType = JdbcType.INTEGER),
+		@Result(column = "patron_type", property = "patronType", jdbcType = JdbcType.CHAR),
+		@Result(column = "fname", property = "firstName", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "mname", property = "middleName", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "lname", property = "lastName", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "login_id", property = "loginId", jdbcType = JdbcType.VARCHAR),
 		
 	})
 	public UserDTO getValidUser(@Param("username") final String userName, @Param("password") final String password);
+
+	@Select("select * from patron where patron_id=#{username}")
+	@Results({
+			@Result(column = "library_id", property = "libraryId", jdbcType = JdbcType.INTEGER),
+			@Result(column = "patron_id", property = "patronId", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "patron_category_id", property = "patronCategoryId", jdbcType = JdbcType.INTEGER),
+			@Result(column = "library_patron_id", property = "libraryPatronId", jdbcType = JdbcType.INTEGER),
+			@Result(column = "patron_type", property = "patronType", jdbcType = JdbcType.CHAR),
+			@Result(column = "fname", property = "firstName", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "mname", property = "middleName", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "lname", property = "lastName", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "login_id", property = "loginId", jdbcType = JdbcType.VARCHAR),
+
+	})
+	public UserDTO getUserById(@Param("username") final String userName);
+
 	
-	
-	@Select("select * from client where client_id=#{client_id} and client_secret=#{client_secret}")
+	@Select("select * from oauth_client where client_id=#{client_id} and client_secret=#{client_secret}")
 	@Results({
 		@Result(column = "client_id", property = "clientID", jdbcType = JdbcType.VARCHAR),
 		@Result(column = "client_secret", property = "clientSecret", jdbcType = JdbcType.VARCHAR),
@@ -49,7 +67,7 @@ public interface OAuth2Mapper {
 	public ClientDTO getValidClient(@Param("client_id") String clientId, @Param("client_secret") final String clientSecret);
 	
 	
-	@Insert("insert into token(client_id,userid,access_token,refresh_token,token_type" +
+	@Insert("insert into oauth_token(client_id,user_id,access_token,refresh_token,token_type" +
 			",scope,auth_code,state,client_type,at_created,rt_created,at_expires_in,rt_expires_in" +
 			",request_source,request_detail) values(#{clientID},#{userID},#{accessToken},#{refreshToken},#{tokenType},#{scope}" +
 			",#{authorizationCode},#{state},#{clientType},#{accessTokenCreateDate ,jdbcType=TIMESTAMP},#{refreshTokenCreatedDate,jdbcType=TIMESTAMP}," +
@@ -58,7 +76,7 @@ public interface OAuth2Mapper {
 	)
 	public void generateToken(TokenDTO tokenDto);
 	
-	@Insert("insert into token_log(client_id,userid,access_token,refresh_token,token_type" +
+	@Insert("insert into oauth_token_log(client_id,user_id,access_token,refresh_token,token_type" +
 			",scope,auth_code,state,client_type,at_created,rt_created,at_expires_in,rt_expires_in" +
 			",request_source,request_detail,token_action) values(#{clientID},#{userID},#{accessToken},#{refreshToken},#{tokenType},#{scope}" +
 			",#{authorizationCode},#{state},#{clientType},#{accessTokenCreateDate ,jdbcType=TIMESTAMP},#{refreshTokenCreatedDate,jdbcType=TIMESTAMP}," +
@@ -67,10 +85,10 @@ public interface OAuth2Mapper {
 	)
 	public void generateTokenLog(TokenLogDTO tokenLogDto);
 	
-	@Select("select * from token where refresh_token=#{refresh_token}")
+	@Select("select * from oauth_token where refresh_token=#{refresh_token}")
 	@Results({
 		@Result(column = "client_id", property = "clientID", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "userid", property = "userID", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "user_id", property = "userID", jdbcType = JdbcType.VARCHAR),
 		@Result(column = "access_token", property = "accessToken", jdbcType = JdbcType.VARCHAR),
 		@Result(column = "refresh_token", property = "refreshToken", jdbcType = JdbcType.VARCHAR),
 		@Result(column = "token_type", property = "tokenType", jdbcType = JdbcType.VARCHAR),
@@ -87,16 +105,16 @@ public interface OAuth2Mapper {
 	})
 	public TokenDTO isRefreshTokenValid(@Param("refresh_token") final String refreshToken);
 	
-	@Update("update token set refresh_token=#{updatedToken.refreshToken}, access_token=#{updatedToken.accessToken}" +
+	@Update("update oauth_token set refresh_token=#{updatedToken.refreshToken}, access_token=#{updatedToken.accessToken}" +
 			", at_expires_in=#{updatedToken.accessTokenExpiredIn},rt_expires_in=#{updatedToken.refreshTokenExpiredIn} where refresh_token=#{old_refresh_token}")
 	public void updateRefreshToken(@Param("updatedToken") final TokenDTO updatedToken,
 			@Param("old_refresh_token") final String existingRefreshToken);
 	
 	
-	@Select("select * from token where access_token=#{access_token}")
+	@Select("select * from oauth_token where access_token=#{access_token}")
 	@Results({
 		@Result(column = "client_id", property = "clientID", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "userid", property = "userID", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "user_id", property = "userID", jdbcType = JdbcType.VARCHAR),
 		@Result(column = "access_token", property = "accessToken", jdbcType = JdbcType.VARCHAR),
 		@Result(column = "refresh_token", property = "refreshToken", jdbcType = JdbcType.VARCHAR),
 		@Result(column = "token_type", property = "tokenType", jdbcType = JdbcType.VARCHAR),
