b5148f29da1ced1179a7887cee6b1456681e48e6
abhiptl <abhishek.patel@sanelib.com>
2016-02-19 14:27:14 +0530

    Adding oauth related tables schema.we have only three tables to provide oauth support.
    oauth_client : This table table stored clients details of NGL.Currently we have only web client
    oauth_token : This table store generated access token and refresh token.
    oauth_token_log : This table store history of access token when new access token is generated from refresh token.

--- oauth/src/main/resources/oauth2-db.sql
+++ oauth/src/main/resources/oauth2-db.sql
@@ -1,91 +1,91 @@
---
--- Table structure for table `token`
---
+-- Table: public.oauth_client
 
-DROP TABLE IF EXISTS `token`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `token` (
-  `client_id` varchar(100) NOT NULL,
-  `userid` varchar(20) DEFAULT NULL,
-  `access_token` varchar(200) NOT NULL,
-  `refresh_token` varchar(200) DEFAULT NULL,
-  `token_type` varchar(30) DEFAULT NULL,
-  `scope` varchar(100) DEFAULT NULL,
-  `auth_code` varchar(200) DEFAULT NULL,
-  `state` varchar(100) DEFAULT NULL,
-  `client_type` varchar(20) DEFAULT NULL,
-  `at_created` datetime DEFAULT NULL,
-  `rt_created` datetime DEFAULT NULL,
-  `at_expires_in` datetime DEFAULT NULL,
-  `rt_expires_in` datetime DEFAULT NULL,
-  `request_source` varchar(20) DEFAULT NULL,
-  `request_detail` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`access_token`),
-  UNIQUE KEY `refresh_token` (`refresh_token`),
-  KEY `IX_user_id` (`userid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- DROP TABLE public.oauth_client;
 
---
--- Table structure for table `client`
---
+CREATE TABLE public.oauth_client
+(
+  client_id character varying(100) NOT NULL,
+  client_secret character varying(100) NOT NULL,
+  client_name character varying(100) NOT NULL,
+  description character varying(200) NOT NULL,
+  client_url character varying(200),
+  client_type character varying(20) NOT NULL,
+  scope character varying(100),
+  redirect_uri character varying(200),
+  regdate date,
+  CONSTRAINT "PK_clientId" PRIMARY KEY (client_id)
+)
+WITH (
+OIDS=FALSE
+);
+ALTER TABLE public.oauth_client
+OWNER TO postgres;
 
-DROP TABLE IF EXISTS `client`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `client` (
-  `client_id` varchar(100) NOT NULL,
-  `client_secret` varchar(100) NOT NULL,
-  `client_name` varchar(300) NOT NULL,
-  `description` varchar(400) NOT NULL,
-  `client_url` varchar(300) NOT NULL,
-  `client_type` varchar(20) NOT NULL,
-  `scope` varchar(300) DEFAULT NULL,
-  `redirect_uri` varchar(400) NOT NULL,
-  `regdate` date DEFAULT NULL,
-  PRIMARY KEY (`client_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- Table: public.oauth_token
 
---
--- Table structure for table `token_log`
---
+-- DROP TABLE public.oauth_token;
 
-DROP TABLE IF EXISTS `token_log`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `token_log` (
-  `client_id` varchar(100) NOT NULL,
-  `userid` varchar(20) DEFAULT NULL,
-  `access_token` varchar(200) NOT NULL,
-  `refresh_token` varchar(200) DEFAULT NULL,
-  `token_type` varchar(30) DEFAULT NULL,
-  `scope` varchar(100) DEFAULT NULL,
-  `auth_code` varchar(200) DEFAULT NULL,
-  `state` varchar(100) DEFAULT NULL,
-  `client_type` varchar(20) DEFAULT NULL,
-  `at_created` datetime DEFAULT NULL,
-  `rt_created` datetime DEFAULT NULL,
-  `at_expires_in` datetime DEFAULT NULL,
-  `rt_expires_in` datetime DEFAULT NULL,
-  `request_source` varchar(20) DEFAULT NULL,
-  `request_detail` varchar(100) DEFAULT NULL,
-  `token_action` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`access_token`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE public.oauth_token
+(
+  client_id character varying(100),
+  user_id character varying(20),
+  access_token character varying(200) NOT NULL,
+  refresh_token character varying(200),
+  token_type character varying(30),
+  scope character varying(100),
+  auth_code character varying(200),
+  state character varying(100),
+  client_type character varying(20),
+  at_created timestamp without time zone,
+  rt_created timestamp without time zone,
+  at_expires_in timestamp without time zone,
+  rt_expires_in timestamp without time zone,
+  request_source character varying(50),
+  request_detail character varying(100),
+  CONSTRAINT "PK_token" PRIMARY KEY (access_token),
+  CONSTRAINT "UK_refresh_token" UNIQUE (refresh_token)
+)
+WITH (
+OIDS=FALSE
+);
+ALTER TABLE public.oauth_token
+OWNER TO postgres;
 
---
--- Table structure for table `users`
---
+-- Index: public."IX_user_id"
 
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users` (
-  `userid` varchar(20) NOT NULL,
-  `username` varchar(40) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  PRIMARY KEY (`userid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- DROP INDEX public."IX_user_id";
+
+CREATE INDEX "IX_user_id"
+ON public.oauth_token
+USING btree
+(user_id COLLATE pg_catalog."default");
+
+-- Table: public.oauth_token_log
+
+-- DROP TABLE public.oauth_token_log;
+
+CREATE TABLE public.oauth_token_log
+(
+  client_id character varying(100),
+  user_id character varying(20),
+  access_token character varying(200) NOT NULL,
+  refresh_token character varying(200),
+  token_type character varying(30),
+  scope character varying(100),
+  auth_code character varying(200),
+  state character varying(100),
+  client_type character varying(20),
+  at_created timestamp without time zone,
+  rt_created timestamp without time zone,
+  at_expires_in timestamp without time zone,
+  rt_expires_in timestamp without time zone,
+  request_source character varying(50),
+  request_detail character varying(100),
+  token_action character varying(10),
+  CONSTRAINT "PK_token_log" PRIMARY KEY (access_token)
+)
+WITH (
+OIDS=FALSE
+);
+ALTER TABLE public.oauth_token_log
+OWNER TO postgres;
