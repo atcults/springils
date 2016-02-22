bc46668a8fd8a982f73255571b6c36d3e4326fc7
abhiptl <abhishek.patel@sanelib.com>
2016-02-22 16:40:17 +0530

    Adding toString method using Apache String builder.Use this toString method log all the attributes inside object.

--- api/src/main/java/org/sanelib/ils/api/services/UserSessionImpl.java
+++ api/src/main/java/org/sanelib/ils/api/services/UserSessionImpl.java
@@ -1,5 +1,8 @@
 package org.sanelib.ils.api.services;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 public class UserSessionImpl implements UserSession {
 
     private String userCode;
@@ -20,4 +23,9 @@ public class UserSessionImpl implements UserSession {
     public void setLibraryId(Integer libraryId) {
         this.libraryId = libraryId;
     }
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+	}
 }
