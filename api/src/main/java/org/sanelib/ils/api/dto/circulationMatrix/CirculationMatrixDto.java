b5a1df4ab4b306584f4629653bfceec5f06400bf
bansisheth <bansi.sheth@sanelib.com>
2016-02-23 17:38:51 +0530

    Updating fields and toString method in DTO (CirculationMatrixDto) of Circulation Matrix entity.

--- api/src/main/java/org/sanelib/ils/api/dto/circulationMatrix/CirculationMatrixDto.java
+++ api/src/main/java/org/sanelib/ils/api/dto/circulationMatrix/CirculationMatrixDto.java
@@ -1,6 +1,10 @@
 package org.sanelib.ils.api.dto.circulationMatrix;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.hibernate.mapping.List;
 import org.sanelib.ils.api.dto.DtoWithLibraryId;
+import org.sanelib.ils.core.enums.DurationType;
 
 public class CirculationMatrixDto implements DtoWithLibraryId {
 
@@ -8,15 +12,25 @@ public class CirculationMatrixDto implements DtoWithLibraryId {
     private String patronCategoryId;
     private String materialTypeId;
     private String withEffectFrom;
-    private String loanInDays;
-    private String loanInHours;
-    private String loanInNextOccurrence;
-    private String loanLimit;
+    private String overAllLoanLimit;
     private String renewalLimit;
     private String finePerDay;
     private String maxCeilOnFine;
-    private String renewalThroughOpac;
+    private String renewalThroughOPAC;
     private String otherDetails;
+    private String auditUserCode;
+    private DurationType loanDurationType;
+    private String loanDuration;
+    private boolean includeHolidaysInDateDue;
+    private List fixedDateDues;
+    private DurationType chargeDurationType;
+    private boolean includeHolidaysInCharges;
+    private String day;
+    private String month;
+    private String from;
+    private String to;
+    private String amount;
+
 
     public void setLibraryId(String libraryId) {
         this.libraryId = libraryId;
@@ -46,36 +60,12 @@ public class CirculationMatrixDto implements DtoWithLibraryId {
         this.withEffectFrom = withEffectFrom;
     }
 
-    public String getLoanInDays() {
-        return loanInDays;
+    public String getOverAllLoanLimit() {
+        return overAllLoanLimit;
     }
 
-    public void setLoanInDays(String loanInDays) {
-        this.loanInDays = loanInDays;
-    }
-
-    public String getLoanInHours() {
-        return loanInHours;
-    }
-
-    public void setLoanInHours(String loanInHours) {
-        this.loanInHours = loanInHours;
-    }
-
-    public String getLoanInNextOccurrence() {
-        return loanInNextOccurrence;
-    }
-
-    public void setLoanInNextOccurrence(String loanInNextOccurrence) {
-        this.loanInNextOccurrence = loanInNextOccurrence;
-    }
-
-    public String getLoanLimit() {
-        return loanLimit;
-    }
-
-    public void setLoanLimit(String loanLimit) {
-        this.loanLimit = loanLimit;
+    public void setOverAllLoanLimit(String overAllLoanLimit) {
+        this.overAllLoanLimit = overAllLoanLimit;
     }
 
     public String getRenewalLimit() {
@@ -102,12 +92,12 @@ public class CirculationMatrixDto implements DtoWithLibraryId {
         this.maxCeilOnFine = maxCeilOnFine;
     }
 
-    public String getRenewalThroughOpac() {
-        return renewalThroughOpac;
+    public String getRenewalThroughOPAC() {
+        return renewalThroughOPAC;
     }
 
-    public void setRenewalThroughOpac(String renewalThroughOpac) {
-        this.renewalThroughOpac = renewalThroughOpac;
+    public void setRenewalThroughOPAC(String renewalThroughOPAC) {
+        this.renewalThroughOPAC = renewalThroughOPAC;
     }
 
     public String getOtherDetails() {
@@ -118,8 +108,109 @@ public class CirculationMatrixDto implements DtoWithLibraryId {
         this.otherDetails = otherDetails;
     }
 
+    public String getAuditUserCode() {
+        return auditUserCode;
+    }
+
+    public void setAuditUserCode(String auditUserCode) {
+        this.auditUserCode = auditUserCode;
+    }
+
+    public DurationType getLoanDurationType() {
+        return loanDurationType;
+    }
+
+    public void setLoanDurationType(DurationType loanDurationType) {
+        this.loanDurationType = loanDurationType;
+    }
+
+    public String getLoanDuration() {
+        return loanDuration;
+    }
+
+    public void setLoanDuration(String loanDuration) {
+        this.loanDuration = loanDuration;
+    }
+
+    public Boolean getIncludeHolidaysInDateDue() {
+        return includeHolidaysInDateDue;
+    }
+
+    public void setIncludeHolidaysInDateDue(boolean includeHolidaysInDateDue) {
+        this.includeHolidaysInDateDue = includeHolidaysInDateDue;
+    }
+
+    public List getFixedDateDues() {
+        return fixedDateDues;
+    }
+
+    public void setFixedDateDues(List fixedDateDues) {
+        this.fixedDateDues = fixedDateDues;
+    }
+
+    public DurationType getChargeDurationType() {
+        return chargeDurationType;
+    }
+
+    public void setChargeDurationType(DurationType chargeDurationType) {
+        this.chargeDurationType = chargeDurationType;
+    }
+
+    public Boolean getIncludeHolidaysInCharges() {
+        return includeHolidaysInCharges;
+    }
+
+    public void setIncludeHolidaysInCharges(boolean includeHolidaysInCharges) {
+        this.includeHolidaysInCharges = includeHolidaysInCharges;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
     @Override
     public String getLibraryId() {
         return libraryId;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
     }
 }
