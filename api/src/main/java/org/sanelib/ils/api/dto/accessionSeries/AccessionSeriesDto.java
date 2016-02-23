6596b1772e0b3f0d919bdaaef28f54e8d8a2fbde
PMSanelib <priya.mehta@sanelib.com>
2016-02-23 23:03:08 +0530

    Adding string builder for Accession Series Dto

--- api/src/main/java/org/sanelib/ils/api/dto/accessionSeries/AccessionSeriesDto.java
+++ api/src/main/java/org/sanelib/ils/api/dto/accessionSeries/AccessionSeriesDto.java
@@ -1,5 +1,7 @@
 package org.sanelib.ils.api.dto.accessionSeries;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 import org.sanelib.ils.api.dto.DtoWithCode;
 import org.sanelib.ils.api.dto.DtoWithLibraryId;
 import org.sanelib.ils.core.enums.AccessionSeriesType;
@@ -77,6 +79,11 @@ public class AccessionSeriesDto implements DtoWithCode, DtoWithLibraryId {
 
     public void setTypeName(AccessionSeriesType  typeName) {
         this.typeName = typeName;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
     }
 }
 
