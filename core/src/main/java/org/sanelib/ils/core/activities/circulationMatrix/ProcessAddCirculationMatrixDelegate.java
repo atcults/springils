893ee6fdb193e54c0985f2bdb86962526910460e
bansisheth <bansi.sheth@sanelib.com>
2016-02-23 17:22:16 +0530

    Updating annotate class in add activity (ProcessAddCirculationMatrixDelegate) of Circulation Matrix entity.

--- core/src/main/java/org/sanelib/ils/core/activities/circulationMatrix/ProcessAddCirculationMatrixDelegate.java
+++ core/src/main/java/org/sanelib/ils/core/activities/circulationMatrix/ProcessAddCirculationMatrixDelegate.java
@@ -10,9 +10,11 @@ import org.sanelib.ils.core.enums.DurationType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.Objects;
 
+@Component
 public class ProcessAddCirculationMatrixDelegate implements JavaDelegate {
 
     private static final Logger LOG = LoggerFactory.getLogger(ProcessAddCirculationMatrixDelegate.class);
