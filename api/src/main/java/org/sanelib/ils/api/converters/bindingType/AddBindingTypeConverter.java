43ff449c0c2f0c501a686d0d85ba3062bbbed8a4
PMSanelib <priya.mehta@sanelib.com>
2016-02-24 13:51:44 +0530

    Adding conditions for price field value in binding type entity

--- api/src/main/java/org/sanelib/ils/api/converters/bindingType/AddBindingTypeConverter.java
+++ api/src/main/java/org/sanelib/ils/api/converters/bindingType/AddBindingTypeConverter.java
@@ -5,11 +5,13 @@ import com.google.common.base.Strings;
 import org.sanelib.ils.api.converters.ConverterHelper;
 import org.sanelib.ils.api.converters.DtoToCommandConverter;
 import org.sanelib.ils.api.dto.bindingType.BindingTypeDto;
-import org.sanelib.ils.common.utils.DateHelper;
+import org.sanelib.ils.common.utils.RegularExpressionHelper;
 import org.sanelib.ils.core.commands.ProcessCommand;
 import org.sanelib.ils.core.commands.bindingType.AddBindingType;
 import org.sanelib.ils.core.exceptions.ProcessError;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 @Component
 public class AddBindingTypeConverter implements DtoToCommandConverter<BindingTypeDto> {
@@ -26,7 +28,17 @@ public class AddBindingTypeConverter implements DtoToCommandConverter<BindingTyp
             command.setBindType(dto.getBindType());
         }
 
-        command.setPrice(Double.parseDouble(dto.getPrice()));
+        if((Objects.equals(dto.getPrice(), "")) ||
+                (!Strings.isNullOrEmpty(dto.getPrice()) && !RegularExpressionHelper.checkDecimal(dto.getPrice()))){
+            processError.addError("common.field.invalidDouble", "price", "domain.bindingType.price");
+        } else if (dto.getPrice() != null) {
+            Double price = Double.parseDouble(dto.getPrice());
+            if(price < 0){
+                processError.addError("common.field.shouldBeGraterOrEqualThan", "price", "domain.bindingType.price");
+            } else {
+                command.setPrice(price);
+            }
+        }
 
         return command;
     }
